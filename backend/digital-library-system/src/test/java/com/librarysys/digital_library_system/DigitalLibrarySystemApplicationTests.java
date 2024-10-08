89f580bf6a442362bb624b7367d6d0acc124de79
AndyGen-CPRO <christianandy.generoso.dl@gmail.com>
2024-10-08 14:14:51 -0600

    added empty Spring Boot Project

---
+++ backend/digital-library-system/src/test/java/com/librarysys/digital_library_system/DigitalLibrarySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.librarysys.digital_library_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalLibrarySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
